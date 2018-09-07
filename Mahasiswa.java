f9cebb78a696c843c28a98f1716a9e9c1ecd2740
Hasin98 <mahasin.anas@gmail.com>
2018-09-07 15:23:15 +0700

    Initialized Commit Dosen.java and Mahasiswa.java

---
+++ Mahasiswa.java
@@ -0,0 +1,30 @@
+
+public class Mahasiswa {
+	int npm;
+	String name;
+	
+	public Mahasiswa(){
+		int npm;
+		String name;
+	}
+
+	public int getNpm() {
+		return npm;
+	}
+
+	public void setNpm(int npm) {
+		this.npm = npm;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+	
+}
